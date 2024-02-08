5ed1ed1a3414b1f8e11d52e63dba28496bb2c394
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-09 04:58:14 +0530

    Create Instance Variable.py

---
+++ Variables/Instance Variable.py
@@ -0,0 +1,10 @@
+# Instance Variable
+class MyClass:
+    def __init__(self, x):
+        self.x = x  # Instance variable
+
+obj1 = MyClass(5)
+obj2 = MyClass(10)
+
+print(obj1.x)  # Output: 5
+print(obj2.x)  # Output: 10
