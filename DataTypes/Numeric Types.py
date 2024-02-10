fa60b21f988eadb91357266276f8aba8fc597f14
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-11 02:23:03 +0530

    Create Numeric Types.py

---
+++ DataTypes/Numeric Types.py
@@ -0,0 +1,10 @@
+#1.Numeric Types:
+#I.int: Integer numbers, e.g., 5, -3, 0.
+x = 5
+y = -3
+z = 0
+
+#II.float: Floating-point numbers, e.g., 3.14, -0.001, 2.0.
+pi = 3.14
+temperature = -0.001
+height = 2.0
