829d620fad371c352b2d0ca4b9f6249ec8a4ea52
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-11 02:24:48 +0530

    Create Set Types.py

---
+++ DataTypes/Set Types.py
@@ -0,0 +1,6 @@
+#5.Set Types:
+#I.set: Unordered collection of unique items, mutable.
+unique_numbers = {1, 2, 3, 4, 5}
+
+#II.frozenset: Unordered collection of unique items, immutable.
+immutable_set = frozenset({1, 2, 3})
