6d6eee7c877546b004b85632e6ff36a07e1fe1d4
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-11 22:14:11 +0530

    Create if-else statement.py

---
+++ DecisionMaking/if-else statement.py
@@ -0,0 +1,8 @@
+# Simple Python Program to check whether a person is eligible to vote or not.  
+age = int (input("Enter your age: "))    
+# Here, we are taking an integer num and taking input dynamically  
+if age>=18:    
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print("You are eligible to vote !!");    
+else:    
+    print("Sorry! you have to wait !!");    
