09b4251155da44e107fa559917760bb6e081e0ce
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-11 22:10:29 +0530

    Create if-statement.py

---
+++ DecisionMaking/if-statement.py
@@ -0,0 +1,6 @@
+# Simple Python program to understand the if statement  
+num = int(input("enter the number:"))         
+# Here, we are taking an integer num and taking input dynamically  
+if num%2 == 0:      
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print("The Given number is an even number")    
