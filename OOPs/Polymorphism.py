3afc47cf317a64af5cb469f641a96227d36c5334
Mr. Raghav Kumar Singh <104582254+19csd26@users.noreply.github.com>
2024-02-12 23:08:24 +0530

    Update Polymorphism.py

--- OOPs/Polymorphism.py
+++ OOPs/Polymorphism.py
@@ -28,11 +28,10 @@ print(animal_sounds(dog))  # Output: Woof!
 print(animal_sounds(cat))  # Output: Meow!
 print(animal_sounds(cow))  # Output: Moo!
 
-'''
+"""
 In this example:
-
 We have a base class Animal with a method make_sound(), which is overridden by subclasses Dog, Cat, and Cow.
 The animal_sounds() function takes an object of type Animal (or any subclass of Animal) as a parameter and calls its make_sound() method.
 When we pass different animal objects to the animal_sounds() function, polymorphism allows the correct make_sound() method of each object to be called based on its actual type.
 Even though animal_sounds() function is only defined once, it behaves differently depending on the type of object passed to it, demonstrating polymorphic behavior.
-'''
+"""
